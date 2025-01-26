bec36da76fd3654e878fef15b3e722861d5774b9
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-01-26 13:54:04 +0100

    lib and types formatting

--- frontend/src/lib/parser.ts
+++ frontend/src/lib/parser.ts
@@ -2,22 +2,22 @@ import { Password } from '@/types';
 
 // lib/csvParser.ts
 export const parseCSV = (file: File): Promise<Password[]> => {
-     return new Promise((resolve, reject) => {
-          const reader = new FileReader();
-          reader.onload = (event) => {
-               const text = event.target?.result as string;
-               const rows = text.split('\n').map((row) => row.split(','));
-               const passwords = rows.map((row) => ({
-                    id: '',
-                    title: row[0],
-                    username: row[1],
-                    password: row[2],
-                    createdAt: new Date().toISOString(),
-                    updatedAt: new Date().toISOString(),
-               }));
-               resolve(passwords);
-          };
-          reader.onerror = (error) => reject(error);
-          reader.readAsText(file);
-     });
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.onload = (event) => {
+			const text = event.target?.result as string;
+			const rows = text.split('\n').map((row) => row.split(','));
+			const passwords = rows.map((row) => ({
+				id: '',
+				title: row[0],
+				username: row[1],
+				password: row[2],
+				createdAt: new Date().toISOString(),
+				updatedAt: new Date().toISOString(),
+			}));
+			resolve(passwords);
+		};
+		reader.onerror = (error) => reject(error);
+		reader.readAsText(file);
+	});
 };
