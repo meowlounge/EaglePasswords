2120343c732017c1559ae61a945776687038a290
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-01-26 13:56:57 +0100

    page formatted

--- frontend/src/app/auth/callback/page.tsx
+++ frontend/src/app/auth/callback/page.tsx
@@ -10,73 +10,72 @@ import { getBaseApiUrl, fetchUserById } from '@/lib/api';
 import { User } from '@/types';
 
 const DiscordCallback = () => {
-     const router = useRouter();
-     const [error, setError] = useState<string | null>(null);
-     const [user, setUser] = useState<User | null>(null);
+	const router = useRouter();
+	const [error, setError] = useState<string | null>(null);
+	const [user, setUser] = useState<User | null>(null);
 
-     useEffect(() => {
-          const fetchToken = async () => {
-               try {
-                    const token =
-                         new URLSearchParams(window.location.search).get(
-                              'token'
-                         ) || null;
-                    if (!token) {
-                         setError('Kein Authentifizierungstoken vorhanden.');
-                         return;
-                    }
+	useEffect(() => {
+		const fetchToken = async () => {
+			try {
+				const token =
+					new URLSearchParams(window.location.search).get('token') ||
+					null;
+				if (!token) {
+					setError('Kein Authentifizierungstoken vorhanden.');
+					return;
+				}
 
-                    document.cookie = `eagletoken=${token}; Expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; secure; SameSite=Strict`;
+				document.cookie = `eagletoken=${token}; Expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; secure; SameSite=Strict`;
 
-                    const userData = await fetchUserById(token);
-                    setUser(userData);
-                    router.push('/');
-               } catch (error) {
-                    setError(
-                         'Fehler beim Verarbeiten des Tokens. Bitte versuchen Sie es erneut.'
-                    );
-               }
-          };
+				const userData = await fetchUserById(token);
+				setUser(userData);
+				router.push('/');
+			} catch (error) {
+				setError(
+					'Fehler beim Verarbeiten des Tokens. Bitte versuchen Sie es erneut.'
+				);
+			}
+		};
 
-          fetchToken();
-     }, [router]);
+		fetchToken();
+	}, [router]);
 
-     if (error) {
-          return (
-               <div className='flex h-screen items-center justify-center bg-neutral-900 text-center p-4'>
-                    <div className='w-full max-w-md bg-neutral-800 p-8 rounded-xl shadow-lg border border-neutral-600'>
-                         <h2 className='text-3xl font-bold text-red-500 mb-4'>
-                              Fehler!
-                         </h2>
-                         <p className='text-neutral-300 mb-6'>{error}</p>
-                         <Button
-                              onClick={() =>
-                                   (window.location.href = `${getBaseApiUrl()}/api/auth`)
-                              }
-                              variant='danger'
-                              className='w-full'
-                              icon={RefreshCcw}
-                              content='Erneut Anmelden'
-                         ></Button>
-                    </div>
-               </div>
-          );
-     }
+	if (error) {
+		return (
+			<div className='flex h-screen items-center justify-center bg-neutral-900 text-center p-4'>
+				<div className='w-full max-w-md bg-neutral-800 p-8 rounded-xl shadow-lg border border-neutral-600'>
+					<h2 className='text-3xl font-bold text-red-500 mb-4'>
+						Fehler!
+					</h2>
+					<p className='text-neutral-300 mb-6'>{error}</p>
+					<Button
+						onClick={() =>
+							(window.location.href = `${getBaseApiUrl()}/api/auth`)
+						}
+						variant='danger'
+						className='w-full'
+						icon={RefreshCcw}
+						content='Erneut Anmelden'
+					></Button>
+				</div>
+			</div>
+		);
+	}
 
-     return (
-          <div className='flex h-screen items-center justify-center bg-neutral-900 text-center p-4'>
-               <div className='w-full max-w-md bg-neutral-800 p-8 rounded-xl shadow-lg border border-neutral-600'>
-                    <h2 className='text-3xl font-bold text-neutral-100 mb-4'>
-                         Authentifizierung wird verarbeitet...
-                    </h2>
-                    <h1>Welcome {user?.username}</h1>
-                    <Spinner className='h-12 w-12 text-neutral-500 mx-auto mb-6' />
-                    <p className='text-neutral-300'>
-                         Bitte warten Sie einen Moment...
-                    </p>
-               </div>
-          </div>
-     );
+	return (
+		<div className='flex h-screen items-center justify-center bg-neutral-900 text-center p-4'>
+			<div className='w-full max-w-md bg-neutral-800 p-8 rounded-xl shadow-lg border border-neutral-600'>
+				<h2 className='text-3xl font-bold text-neutral-100 mb-4'>
+					Authentifizierung wird verarbeitet...
+				</h2>
+				<h1>Welcome {user?.username}</h1>
+				<Spinner className='h-12 w-12 text-neutral-500 mx-auto mb-6' />
+				<p className='text-neutral-300'>
+					Bitte warten Sie einen Moment...
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default DiscordCallback;
--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -15,212 +15,198 @@ import { useSettingsPage } from '@/hooks/useSettingsPage';
 import { enableTwoFactorAuth, verifyTwoFactorCode } from '@/lib/api';
 import { toast } from 'sonner';
 import { Spinner } from '@/components/ui/Spinner';
-import { Tooltip } from '@/components/ui/Tooltip';
 
 const SettingsPage = () => {
-     const { passwords } = useHomePageLogic();
-     const {
-          user,
-          loading,
-          twoFASecret,
-          is2FADialogOpen,
-          isImportDialogOpen,
-          isExportDialogOpen,
-          isChangePasswordDialogOpen,
-          isDeleteAccountDialogOpen,
-          setIsDeleteAccountDialogOpen,
-          setIs2FADialogOpen,
-          setIsImportDialogOpen,
-          setIsExportDialogOpen,
-          setIsChangePasswordDialogOpen,
-          // handleEnable2FA,
-          // handleDisable2FA,
-          handleDeleteAccount,
-          setError,
-          handleImportSuccess,
-     } = useSettingsPage();
+	const { passwords } = useHomePageLogic();
+	const {
+		user,
+		loading,
+		twoFASecret,
+		is2FADialogOpen,
+		isImportDialogOpen,
+		isExportDialogOpen,
+		isChangePasswordDialogOpen,
+		isDeleteAccountDialogOpen,
+		setIsDeleteAccountDialogOpen,
+		setIs2FADialogOpen,
+		setIsImportDialogOpen,
+		setIsExportDialogOpen,
+		setIsChangePasswordDialogOpen,
+		// handleEnable2FA,
+		// handleDisable2FA,
+		handleDeleteAccount,
+		setError,
+		handleImportSuccess,
+	} = useSettingsPage();
 
-     return (
-          <div className='dark:bg-neutral-900 not-dark:bg-neutral-100 flex items-center justify-center min-h-screen'>
-               <div className='max-w-4xl mx-auto p-6 w-full mt-13'>
-                    <div className='space-y-6'>
-                         <div className='bg-neutral-100 dark:bg-neutral-950/50 backdrop-blur-xl rounded-xl border border-neutral-300 dark:border-neutral-800/50 p-6'>
-                              <h2 className='text-lg font-semibold text-neutral-800 mb-4 dark:text-neutral-100'>
-                                   Account Settings
-                              </h2>
-                              {loading ? (
-                                   <div className='flex items-center justify-center space-x-4'>
-                                        <Spinner
-                                             size='w-12 h-12'
-                                             color='text-green-600'
-                                             strokeWidth={5}
-                                             speed='2s'
-                                        />
-                                        <div className='text-xl font-semibold text-neutral-600 dark:text-neutral-400'>
-                                             Loading account settings...
-                                        </div>
-                                   </div>
-                              ) : (
-                                   <div className='flex items-center gap-4 mb-6'>
-                                        <UserAvatar
-                                             username={user?.username || ''}
-                                             id={user?.id || ''}
-                                             avatar={user?.avatar}
-                                        />
-                                        <div>
-                                             <div className='font-medium dark:text-neutral-100'>
-                                                  @
-                                                  {user?.username ||
-                                                       'Unknown User'}
-                                             </div>
-                                             <TimeStamp
-                                                  extended
-                                                  text='joined'
-                                                  timestamp={
-                                                       user?.createdAt || ''
-                                                  }
-                                             />
-                                        </div>
-                                   </div>
-                              )}
-                              <div>
-                                   <Button
-                                        content='Delete Account'
-                                        icon={Trash2}
-                                        onClick={() =>
-                                             setIsDeleteAccountDialogOpen(true)
-                                        }
-                                        variant='danger'
-                                        className='w-full'
-                                   />
-                              </div>
-                         </div>
+	return (
+		<div className='dark:bg-neutral-900 not-dark:bg-neutral-100 flex items-center justify-center min-h-screen'>
+			<div className='max-w-4xl mx-auto p-6 w-full mt-13'>
+				<div className='space-y-6'>
+					<div className='bg-neutral-100 dark:bg-neutral-950/50 backdrop-blur-xl rounded-xl border border-neutral-300 dark:border-neutral-800/50 p-6'>
+						<h2 className='text-lg font-semibold text-neutral-800 mb-4 dark:text-neutral-100'>
+							Account Settings
+						</h2>
+						{loading ? (
+							<div className='flex items-center justify-center space-x-4'>
+								<Spinner
+									size='w-12 h-12'
+									color='text-green-600'
+									strokeWidth={5}
+									speed='2s'
+								/>
+								<div className='text-xl font-semibold text-neutral-600 dark:text-neutral-400'>
+									Loading account settings...
+								</div>
+							</div>
+						) : (
+							<div className='flex items-center gap-4 mb-6'>
+								<UserAvatar
+									username={user?.username || ''}
+									id={user?.id || ''}
+									avatar={user?.avatar}
+								/>
+								<div>
+									<div className='font-medium dark:text-neutral-100'>
+										@{user?.username || 'Unknown User'}
+									</div>
+									<TimeStamp
+										extended
+										text='joined'
+										timestamp={user?.createdAt || ''}
+									/>
+								</div>
+							</div>
+						)}
+						<div>
+							<Button
+								content='Delete Account'
+								icon={Trash2}
+								onClick={() =>
+									setIsDeleteAccountDialogOpen(true)
+								}
+								variant='danger'
+								className='w-full'
+							/>
+						</div>
+					</div>
 
-                         <div className='bg-neutral-100 dark:bg-neutral-950/50 backdrop-blur-xl rounded-xl border border-neutral-300 dark:border-neutral-800/50 p-6'>
-                              <h2 className='text-lg font-semibold text-neutral-800 mb-4 dark:text-neutral-100'>
-                                   Security
-                              </h2>
-                              <div className='space-y-4'>
-                                   <div className='flex items-center justify-between'>
-                                        <div>
-                                             <div className='font-medium dark:text-neutral-100'>
-                                                  Two-Factor Authentication
-                                             </div>
-                                             <div className='text-sm text-neutral-600 dark:text-neutral-400'>
-                                                  Add an extra layer of security
-                                             </div>
-                                        </div>
-                                        <Button
-                                             icon={Shield}
-                                             content={
-                                                  user?.twoFactorEnabled
-                                                       ? 'Disable'
-                                                       : 'Enable'
-                                             }
-                                             // onClick={user?.twoFactorEnabled ? handleDisable2FA : handleEnable2FA}
-                                             disabled
-                                        />
-                                   </div>
-                                   <div className='flex items-center justify-between'>
-                                        <div>
-                                             <div className='font-medium dark:text-neutral-100'>
-                                                  Master Password
-                                             </div>
-                                             <div className='text-sm text-neutral-600 dark:text-neutral-400'>
-                                                  Change your master password
-                                             </div>
-                                        </div>
-                                        <Button
-                                             onClick={() =>
-                                                  setIsChangePasswordDialogOpen(
-                                                       true
-                                                  )
-                                             }
-                                             icon={Lock}
-                                             content='Change'
-                                             variant='border'
-                                             disabled
-                                        />
-                                   </div>
-                              </div>
-                         </div>
+					<div className='bg-neutral-100 dark:bg-neutral-950/50 backdrop-blur-xl rounded-xl border border-neutral-300 dark:border-neutral-800/50 p-6'>
+						<h2 className='text-lg font-semibold text-neutral-800 mb-4 dark:text-neutral-100'>
+							Security
+						</h2>
+						<div className='space-y-4'>
+							<div className='flex items-center justify-between'>
+								<div>
+									<div className='font-medium dark:text-neutral-100'>
+										Two-Factor Authentication
+									</div>
+									<div className='text-sm text-neutral-600 dark:text-neutral-400'>
+										Add an extra layer of security
+									</div>
+								</div>
+								<Button
+									icon={Shield}
+									content={
+										user?.twoFactorEnabled
+											? 'Disable'
+											: 'Enable'
+									}
+									// onClick={user?.twoFactorEnabled ? handleDisable2FA : handleEnable2FA}
+									disabled
+								/>
+							</div>
+							<div className='flex items-center justify-between'>
+								<div>
+									<div className='font-medium dark:text-neutral-100'>
+										Master Password
+									</div>
+									<div className='text-sm text-neutral-600 dark:text-neutral-400'>
+										Change your master password
+									</div>
+								</div>
+								<Button
+									onClick={() =>
+										setIsChangePasswordDialogOpen(true)
+									}
+									icon={Lock}
+									content='Change'
+									variant='border'
+									disabled
+								/>
+							</div>
+						</div>
+					</div>
 
-                         <div className='bg-neutral-100 dark:bg-neutral-950/50 backdrop-blur-xl rounded-xl border border-neutral-300 dark:border-neutral-800/50 p-6'>
-                              <h2 className='text-lg font-semibold text-neutral-800 mb-4 dark:text-neutral-100'>
-                                   Data Management
-                              </h2>
-                              <div className='space-y-4'>
-                                   <Button
-                                        onClick={() =>
-                                             setIsExportDialogOpen(true)
-                                        }
-                                        icon={Download}
-                                        content='Export Passwords'
-                                        className='w-full'
-                                   ></Button>
-                                   <Button
-                                        onClick={() =>
-                                             setIsImportDialogOpen(true)
-                                        }
-                                        icon={Upload}
-                                        content='Import Passwords'
-                                        className='w-full'
-                                   ></Button>
-                              </div>
-                         </div>
-                    </div>
+					<div className='bg-neutral-100 dark:bg-neutral-950/50 backdrop-blur-xl rounded-xl border border-neutral-300 dark:border-neutral-800/50 p-6'>
+						<h2 className='text-lg font-semibold text-neutral-800 mb-4 dark:text-neutral-100'>
+							Data Management
+						</h2>
+						<div className='space-y-4'>
+							<Button
+								onClick={() => setIsExportDialogOpen(true)}
+								icon={Download}
+								content='Export Passwords'
+								className='w-full'
+							></Button>
+							<Button
+								onClick={() => setIsImportDialogOpen(true)}
+								icon={Upload}
+								content='Import Passwords'
+								className='w-full'
+							></Button>
+						</div>
+					</div>
+				</div>
 
-                    <TwoFactorDialog
-                         isOpen={is2FADialogOpen}
-                         secret={twoFASecret || ''}
-                         onClose={() => setIs2FADialogOpen(false)}
-                         onSubmit={async (code) => {
-                              const result = await verifyTwoFactorCode(code);
-                              if (result) {
-                                   const enableResult =
-                                        await enableTwoFactorAuth();
-                                   if (enableResult) {
-                                        toast.success(
-                                             '2FA enabled successfully.'
-                                        );
-                                        setIs2FADialogOpen(false);
-                                   } else {
-                                        setError(
-                                             'Failed to enable 2FA. Please try again later.'
-                                        );
-                                   }
-                              } else {
-                                   setError('Invalid 2FA code.');
-                              }
-                         }}
-                    />
+				<TwoFactorDialog
+					isOpen={is2FADialogOpen}
+					secret={twoFASecret || ''}
+					onClose={() => setIs2FADialogOpen(false)}
+					onSubmit={async (code) => {
+						const result = await verifyTwoFactorCode(code);
+						if (result) {
+							const enableResult = await enableTwoFactorAuth();
+							if (enableResult) {
+								toast.success('2FA enabled successfully.');
+								setIs2FADialogOpen(false);
+							} else {
+								setError(
+									'Failed to enable 2FA. Please try again later.'
+								);
+							}
+						} else {
+							setError('Invalid 2FA code.');
+						}
+					}}
+				/>
 
-                    <DeleteAccountDialog
-                         isOpen={isDeleteAccountDialogOpen}
-                         onClose={() => setIsDeleteAccountDialogOpen(false)}
-                         onConfirm={handleDeleteAccount}
-                         itemType='Account'
-                    />
+				<DeleteAccountDialog
+					isOpen={isDeleteAccountDialogOpen}
+					onClose={() => setIsDeleteAccountDialogOpen(false)}
+					onConfirm={handleDeleteAccount}
+					itemType='Account'
+				/>
 
-                    <DataExporter
-                         passwords={passwords}
-                         isOpen={isExportDialogOpen}
-                         onClose={() => setIsExportDialogOpen(false)}
-                    />
+				<DataExporter
+					passwords={passwords}
+					isOpen={isExportDialogOpen}
+					onClose={() => setIsExportDialogOpen(false)}
+				/>
 
-                    <DataImporter
-                         isOpen={isImportDialogOpen}
-                         onClose={() => setIsImportDialogOpen(false)}
-                         onImportSuccess={handleImportSuccess}
-                    />
+				<DataImporter
+					isOpen={isImportDialogOpen}
+					onClose={() => setIsImportDialogOpen(false)}
+					onImportSuccess={handleImportSuccess}
+				/>
 
-                    <ChangeMasterPasswordDialog
-                         isOpen={isChangePasswordDialogOpen}
-                         onClose={() => setIsChangePasswordDialogOpen(false)}
-                    />
-               </div>
-          </div>
-     );
+				<ChangeMasterPasswordDialog
+					isOpen={isChangePasswordDialogOpen}
+					onClose={() => setIsChangePasswordDialogOpen(false)}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default SettingsPage;
